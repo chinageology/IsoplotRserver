003cd974fa15f0c8599e4aec41b539e61f200d94
CycleUser <cycleuser@cycleuser.org>
2020-02-19 22:23:39 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,19 +1,18 @@
-
-# i. update IsoplotR from GitHub:
-sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotR',force=TRUE)\""
-
-# ii. clone IsoplotRgui from GitHub to /tmp:
-cd /tmp
-git clone https://github.com/pvermees/IsoplotRgui
-
-# iii. copy the app to the shiny-server directory:
-cd IsoplotRgui/inst/shiny-examples/myapp
-sudo cp -R www /srv/shiny-server/IsoplotR*y
-sudo cp -R server.R /srv/shiny-server/IsoplotR*y
-sudo cp -R www /srv/shiny-server/IsoplotR
-sudo cp -R server.R /srv/shiny-server/IsoplotR
-
-# iv. clean up and restart shiny-server
-sudo rm -rf /tmp/IsoplotRgui
-#sudo systemctl restart shiny-server
-#shiny-server
+# i. copy IsoplotR and IsoplotRgui from the London server:                                          
+cd /tmp                                                                                            
+wget http://pieter-vermeesch.es.ucl.ac.uk/IsoplotR.zip                                              
+wget http://pieter-vermeesch.es.ucl.ac.uk/IsoplotRgui.zip                                          
+                                                                                                   
+# ii. install IsoplotR with devtools:                                                              
+sudo su - -c "R -e \"devtools::install_local('/tmp/IsoplotR.zip',force=TRUE)\""                    
+                                                                                                   
+# iii. unzip IsoplotRgui and copy to the shiny-server directory:                                    
+unzip IsoplotRgui                                                                                  
+cd IsoplotRgui-master/inst/shiny-examples/myapp                                                    
+sudo cp -R www /srv/shiny-server/IsoplotR                                                          
+sudo cp -R server.R /srv/shiny-server/IsoplotR                                                      
+                                                                                                   
+# iv. clean up and restart shiny-server                                                            
+cd /tmp                                                                                            
+sudo rm -rf IsoplotR.zip IsoplotRgui.zip IsoplotRgui-master                                        
+sudo systemctl restart shiny-server  
